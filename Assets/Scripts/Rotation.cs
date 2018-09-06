2dee84489ecc392a87141a20ac5e6830a4191d28
Dmitry-Neanderslavski <nzemtzov@hotmail.com>
2018-09-06 09:18:17 -0700

    First commit

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour {
+	public int RotateSpeed;
+	public AudioSource CollectSound;
+	public GameObject TheGem;
+
+	void Update () {
+		transform.Rotate(0, RotateSpeed, 0, Space.World);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		CollectSound.Play();
+		TheGem.SetActive(false);
+	}
+}
